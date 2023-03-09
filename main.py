58cf21d8a45eeb82b56b8d40f57de32fe4bf07d6
Serikbol <serikboly@gmail.com>
2023-03-09 17:42:32 +0100

    Refactoring and CSV implementation

--- main.py
+++ main.py
@@ -3,88 +3,100 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException
 import sys
+import csv
 
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+def decline_cookies(driver):
+    try:
+        wait = WebDriverWait(driver, 10)
+        cookie_decline = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[7]/div[4]/div[3]")))
+        cookie_decline.click()
+    except:
+        print("Cookies banner not found or unable to decline cookies.")
 
-# service = Service(executable_path="/path/to/chromedriver")
-# driver = webdriver.Chrome(service=service)
-url = "https://cz.careers.veeam.com/vacancies"
-
-driver.get(url)
-
-driver.maximize_window()
-
-wait = WebDriverWait(driver, 10)
-
-chosenDepartment = "Research & Development"
-
-chosenLanguage = "English"
-
-element = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[1]/div/div/div[1]/div/div[2]')))
-
-departmentDropDown = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[1]/div/div/div[1]/div/div[2]')
-
-departmentDropDown.click() 
-
-departmentSelect = departmentDropDown.find_elements(By.CSS_SELECTOR, 'a.dropdown-item')
-departmentList = []
-for department in departmentSelect:
-
-    departmentList.append(department.text)
-
-for department in departmentSelect:
+def get_department_list():
+    
+    department_dropdown.click()
+    department_select = department_dropdown.find_elements(By.TAG_NAME, 'a')
+    # department_list = [department.text for department in department_select]
+    department_list = [department.text.rstrip() for department in department_select] # Needed for Product Management Department, as there is a stray &nbsp whitespace preceding it
+    return department_list
 
 
-    if chosenDepartment in departmentList:
-        try:
-            wait.until(EC.element_to_be_clickable(department))
-            is_clickable = True
-        except:
-            is_clickable = False
-        if department.text == chosenDepartment and is_clickable == True:
-            department.click()
+def select_department(chosen_department):
+    department_list = get_department_list()
+    if chosen_department not in department_list:
+        sys.exit(f"The {chosen_department} department is not listed.")
+    for department in department_list:
+        if department == chosen_department:
+            department_select = driver.find_element(By.LINK_TEXT, department)
+            try:
+                wait.until(EC.element_to_be_clickable(department_select))
+                department_select.click()
+                # listing_column = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div/div[2]/div')
+                # listings = listing_column.find_elements(By.XPATH, './*')
+                # listing_count = len(listings)
+                break
+            except ElementClickInterceptedException:
+                # listing_count = 0
+                department_dropdown.click()
+                break
+    # return listing_count
+
+def get_language_list():
+    language_dropdown = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div/div[1]/div/div[3]/div/div/button')
+    language_dropdown.click()
+    lang = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div/div[1]/div/div[3]/div/div/div')
+    language_select = lang.find_elements(By.TAG_NAME, 'div')
+    language_list = []
+    for language in language_select:
+        name = language.find_element(By.CSS_SELECTOR, 'label.custom-control-label').text
+        language_list.append(name)
+    return language_list
+
+
+def select_language(chosen_language):
+    language_list = get_language_list()
+    if chosen_language not in language_list:
+        sys.exit(f"No available language by the name of {chosen_language}.")
+    for language in language_list:
+        if language == chosen_language:
+            language_select = driver.find_element(By.XPATH, f"//label[text()='{language}']/preceding-sibling::input")
+            language_select.click()
             break
-        elif department.get_attribute('class') == "text-muted disabled dropdown-item":
-            sys.exit(f"The {chosenDepartment} department has no available vacancies.")
-        else:
-            pass # TODO find way to display print(f'No Available Department by the name of {department}') only once, if no matches found
-    else:
-        sys.exit(f'The {chosenDepartment} department is not listed')
-
-
-cookieDecline = driver.find_element(By.XPATH, '/html/body/div[7]/div[4]/div[3]')
-cookieDecline.click()
-
-languageDropDown = driver.find_element(By.XPATH,'/html/body/div[1]/div/div[1]/div/div/div[1]/div/div[3]/div/div/button')
-languageDropDown.click()
-
-lang = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div/div[1]/div/div[3]/div/div/div')
-languageSelect = lang.find_elements(By.TAG_NAME, 'div')
-
-for language in languageSelect: #TODO create a function/method for this iteration over the lists, rather than repeating this 
-    name = language.find_element(By.CSS_SELECTOR, 'label.custom-control-label').text
-    button = language.find_element(By.CSS_SELECTOR, 'input.custom-control-input')
-    if name == chosenLanguage:
-        button.click()
-        break
-    else:
-        pass # TODO find way to display print(f'No Available language by the name of {language}') only once, if no matches found
-
-listingColumn = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div/div[2]/div')
-listings = listingColumn.find_elements(By.XPATH, './*')
-
-listingCount = len(listings)
-
-vacanciesOpen = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/h3/span')
-
-
-print(listingCount)
-time.sleep(200)
 
 
 
-# driver.quit()
+with open('input.csv', 'r') as input_file, open('output.csv', 'w', newline='') as output_file:
+    reader = csv.reader(input_file)
+    writer = csv.writer(output_file)
+    writer.writerow(['Department', 'Language', 'Listing Count'])
+
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    url = "https://cz.careers.veeam.com/vacancies"
+    driver.get(url)
+    driver.maximize_window()
+    wait = WebDriverWait(driver, 10)
+    department_dropdown = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div/div[1]/div/div[2]')
+
+    decline_cookies(driver)
+
+    next(reader) # Skip Header values
+
+    for row in reader:
+        chosen_department = row[0]
+        chosen_language = row[1]
+
+        select_department(chosen_department)
+        # cookie_decline = driver.find_element(By.XPATH, '/html/body/div[7]/div[4]/div[3]')
+        # cookie_decline.click()
+        select_language(chosen_language)
+        listing_column = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div/div[2]/div')
+        listings = listing_column.find_elements(By.XPATH, './*')
+        listing_count = len(listings)
+        writer.writerow([chosen_department, chosen_language, listing_count])
+
+    driver.quit()
